0e4958eb3683237a36791899510387ef70d3e8cf
Basil Titovchenko <unibasil@gmail.com>
2018-04-28 17:58:25 +1000

    Porting to JS start

---
+++ nde-data-types.js
@@ -0,0 +1,225 @@
+/**
+ * NDE (Nullsoft Database Engine) in PHP
+ * By Daniel15 - http://www.d15.biz/
+ */
+
+const {	NDEIndex, NDEDatabase, NDEData } = require('nde-data-types.js');
+
+/**
+ * A record, our internal representation. Nothing's here, look at* NDEDatabase::__construct
+ */
+class NDERecord {
+}
+
+/**
+ * A NDE Field Format information (from Winamp SDK):
+ ==================================================================================================
+ Offset                      Data Type      Size                  Field
+ ==================================================================================================
+ 0                           UCHAR          1                     Column ID
+ 1                           UCHAR          1                     Field Type
+ 2                           INT            4                     Size of field data
+ 6                           INT            4                     Next field position in table data pool
+ 10                          INT            4                     Previous field position in table data pool
+ 14                          FIELDDATA      SizeOfFieldData       Field data
+ ==================================================================================================
+ */
+
+const FIELD_UNDEFINED = 255;
+const FIELD_COLUMN = 0;
+const FIELD_INDEX = 1;
+const FIELD_REDIRECTOR = 2;
+const FIELD_STRING = 3;
+const FIELD_INTEGER = 4;
+const FIELD_BOOLEAN = 5;
+const FIELD_BINARY = 6;
+const FIELD_GUID = 7;
+const FIELD_FLOAT = 9;
+const FIELD_DATETIME = 10;
+const FIELD_LENGTH = 11;
+
+class NDEField {
+	id;
+	type;
+	size;
+	next;
+	prev;
+	raw;
+	data;
+
+	/**
+	 * Creates a new NDEField.
+	 * When an instance of this class is created, we need to:
+	 * 1. Get all the data (using the format information as shown above)
+	 * 2. Set the "$data" variable based on the type of field this is.
+	 */
+	constructor(data) {
+		// First two things are unsigned characters (UCHAR).
+		this.id = data.readUInt8(0);
+		this.type = data.readUInt8(1);
+
+		// Next three are integers.
+		this.size = data.readUInt32BE(2);
+		this.next = data.readUInt32BE(6);
+		this.prev = data.readUInt32BE(10);
+
+		// And this is the rest of the data.
+		this.raw = data.readAsArrayBuffer(data, 14, this.size);
+
+		NDEDatabase.debug_msg('&rarr; <strong>Field:</strong> Column ID: ' + this.id + ' . type: ' + this.type +
+			', size: ' + this.size + ', next: ' + this.next + ', prev: ' + this.prev + '<br />');
+
+		// Actually get the data, depending on type.
+		switch (this.type) {
+			case FIELD_COLUMN:
+				this.data = new NDEField_Column(this.raw);
+				break;
+
+			// I don't actually know what these are, so they're ignored for now.
+			case FIELD_INDEX:
+				break;
+
+			case FIELD_STRING:
+				this.data = new NDEField_String(this.raw);
+				break;
+
+			case FIELD_INTEGER:
+			case FIELD_LENGTH:
+				this.data = new NDEField_Integer(this.raw);
+				break;
+
+			case FIELD_DATETIME:
+				this.data = new NDEField_DateTime(this.raw);
+				break;
+
+			// Shouldn't really happen. Yes, I know I haven't implemented all
+			// the different types, but the above ones are the only ones that
+			// seem to be used in the media library.
+			default:
+				console.error(`<strong style="color: red">Unknown field type: ${this.type}</strong>`);
+				break;
+		}
+	}
+
+	/**
+	 * When we convert this to a string, we use the inner type.
+	 * Hopefully that's a good assumption.
+	 */
+	toString() {
+		return this.data.toString();
+	}
+}
+
+/**
+ * All data types inherit from this class
+ */
+class NDEField_Data {
+}
+
+/**
+ * NDE "Column" type
+ * Format information:
+ ==================================================================================================
+ Offset                      Data Type      Size                  Field
+ ==================================================================================================
+ 0                           UCHAR          1                     Column Field Type (ie, FIELD_INTEGER)
+ 1                           UCHAR          1                     Index unique values (0/1)
+ 2                           UCHAR          1                     Size of column name string
+ 3                           STRING         SizeOfColumnName      Public name of the column
+ ==================================================================================================
+ */
+class NDEField_Column extends NDEField_Data {
+	type;
+	unique;
+	size;
+	name;
+
+	constructor(data) {
+		super();
+		// Characters (UCHARs)
+		this.type = data.readUInt8(0);
+		this.unique = data.readUInt8(1);
+		this.size = data.readUInt8(2);
+		// Name = rest of the data
+		this.name = substr(data, 3, this.size);
+
+		NDEDatabase::debug_msg(`&mdash;&rarr; <strong>Column:</strong> Type: ${this.type}, size: ${this.size}, name: ${this.name}<br />`);
+	}
+
+	toString() {
+		return this.name;
+	}
+}
+
+/**
+ * NDE "String" type
+ * Format information:
+ ==================================================================================================
+ Offset                      Data Type      Size                  Field
+ ==================================================================================================
+ 0                           USHORT         2                     Size of string
+ 2                           STRING         SizeOfString          String
+ ==================================================================================================
+ */
+class NDEField_String extends NDEField_Data {
+	size;
+	data;
+
+	constructor(data) {
+		super();
+		// Unsigned short
+		this.size = array_pop(unpack('S', $data));
+		// Convert from UTF-16 (what Winamp uses) to UTF-8 (what PHP uses)
+		this.data = iconv('UTF-16', 'UTF-8', substr($data, 2, this.size));
+		NDEDatabase::debug_msg(`&mdash;&rarr; <strong>String:</strong> Size: ${this.size}, data: ${this.data}<br />`);
+	}
+}
+
+/**
+ * NDE "Integer" type. I think this is the simplest one :)
+ * Format information:
+ ==================================================================================================
+ Offset                      Data Type      Size                  Field
+ ==================================================================================================
+ 0                           INT            4                     Integer value
+ ==================================================================================================
+ */
+class NDEField_Integer extends NDEField_Data {
+	data;
+	constructor (data) {
+		super();
+		this.data = array_pop(unpack('i', data));
+		NDEDatabase::debug_msg(`&mdash;&rarr; <strong>Integer:</strong> data: ${this.data}<br />`);
+	}
+	toString() {
+		return this.data;
+	}
+}
+
+/**
+ * NDE "DateTime" type. This is exact same as an integer, except it's treated
+ * as a date/time format. The number is a UNIX timestamp.
+ *
+ * TODO: Add a field for formatted date?
+ */
+class NDEField_DateTime extends NDEField_Data {
+	public data;
+	constructor($data) {
+		super();
+		this.data = array_pop(unpack('i', $data));
+
+		NDEDatabase::debug_msg(`&mdash;&rarr; <strong>DateTime:</strong> data: ${this.data}<br />`);
+	}
+	toString() {
+		return this.data;
+	}
+}
+
+/**
+ * Print binary data as hex (eg. 0xDE 0xAD 0xFF 0x01)
+ */
+function print_binary(stuff) {
+	for ($i = 0; $i < stuff.length; $i++) {
+		console.log('0x' + stuff[$i].toString(16));
+	}
+}
---
+++ nde.js
@@ -0,0 +1,229 @@
+/**
+ * NDE (Nullsoft Database Engine) in PHP
+ * By Daniel15 - http://www.d15.biz/
+ *
+ * References:
+ * - http://gutenberg.free.fr/fichiers/SDK%20Winamp/nde_specs_v1.txt
+ * - http://translate.google.com/translate?hl=en&sl=de&u=http://itavenue.de/java/mlremote-oder-die-winamp-media-library
+ */
+
+	// All the data type stuff is in a separate file
+	// require('data_types.php');
+
+const fs = require('fs');
+
+/**
+ * NDE Index file class
+ */
+class NDEIndex {
+	// This is the very first thing in the file. It's used to verify that the
+	// file actually is a NDE Index.
+	SIGNATURE;
+	record_count;
+	file;
+
+	/**
+	 * When we create this class, we'd better load the file, and check how long it is.
+	 */
+	constructor ($file) {
+		this.SIGNATURE = 'NDEINDEX';
+		this.file = fopen($file, 'rb');
+		// TODO: Check signature is actually valid
+		$temp = fread(this.file, strlen(this.SIGNATURE));
+		NDEDatabase.debug_msg('Index signature: ' + $temp + '<br />');
+		// Number of records in the file
+		this.record_count = array_pop(unpack('I', fread(this.file, 4)));
+		NDEDatabase.debug_msg('Number of records: ' + this.record_count + '<br />');
+		// Bytes that don't seem to do anything
+		$temp = fread(this.file, 4);
+	}
+
+	record_count() {
+		return this.record_count;
+	}
+
+	/**
+	 * Get the next index from the index file.
+	 */
+	getNext() {
+		$data = unpack('I2', fread(this.file, 8));
+		return {
+			'offset': $data[1],
+			'index': $data[2]
+		};
+	}
+}
+
+class NDEFileRecord {
+	fields;
+}
+
+/**
+ * NDE Data file class
+ */
+class NDEData {
+	// This is the very first thing in the file. It's used to verify that the
+	// file actually is a NDE Table.
+	SIGNATURE = 'NDETABLE';
+	private $file;
+	// The order the columns are in. This is defined by the "Column" field,
+	// which is the first one in the file.
+	private $columns;
+
+	/**
+	 * Just load the file and check the signature
+	 */
+	constructor  ($file) {
+		this.file = fopen($file, 'rb');
+		// TODO: Actually check the signature.
+		$temp = fread(this.file, strlen(this.SIGNATURE));
+		NDEDatabase.debug_msg(`Data signature: ${temp}<br />`);
+	}
+
+	/**
+	 * Get a record from the file. One record consists of many fields, in a
+	 * linked list. Firstly, this gets the first field (identified by the offset
+	 * passed to this function) and reads that. Then, it checks if it has a next
+	 * field to go to (the field will contain this data). If so, it goes to that
+	 * field, and reads it. This continues until we have no more fields in the
+	 * record. After that, we check what type of field it is.
+	 *
+	 * The two "other" types are "column" and "index". I don't know what the
+	 * index type actually does, but the "column" type tells us all the
+	 * information stored about songs. The very first record in the file is a
+	 * "column" record, and the second record is a "index" record. The rest of
+	 * the file is all information about songs.
+	 */
+	get_record($offset, $index)
+	{
+		$record = new NDEFileRecord();
+
+	NDEDatabase.debug_msg('<strong>Record:</strong><br />');
+
+	// While we have fields to get
+	do
+	{
+		// Go to this offset
+		fseek(this.file, $offset);
+		// Read some stuff
+		$data = fread(this.file, 14);
+		// Find out the length we need to read from the file
+		$size = array_pop(unpack('i', substr($data, 2, 4)));
+		// Add this data
+		$data .= fread(this.file, $size);
+		// The actual field itself
+		$field = new NDEField($data);
+		$record->fields[] = $field;
+
+		// Do we have another one in this series? Better grab the offset
+		$offset = $field.next;
+	}
+	while ($offset != 0);
+
+	// Is this the "column" field?
+	if ($record->fields[0]->type == NDEField::FIELD_COLUMN)
+	{
+		// We need to fill our $columns variable!
+		foreach ($record->fields as $field)
+		this.columns[$field->id] = $field->data->name;
+
+		return false;
+	}
+	// otherwise, it could be that weird index one.
+	elseif ($record->fields[0]->type == NDEField::FIELD_INDEX)
+	{
+		// TODO: Find out what this field actually is.
+		return false;
+	}
+
+	// Otherwise, it's a song!
+	// We need to store all the data stuffs
+	$song = new NDERecord();
+
+	foreach ($record->fields as $field)
+	{
+		$variable = this.columns[$field->id];
+		$song->$variable = $field->data->data;
+	}
+
+	return $song;
+}
+}
+
+/**
+ * NDE Database
+ *
+ * A database is basically an index file, and a data file. The index file tells
+ * us where to go to get data, and the data file tells us where the data
+ * actually is. This class coordinates the two files.
+ */
+class NDEDatabase
+{
+	// Files
+	$index;
+	$data;
+
+	// Set this to true to get too much debugging info (go on, try it). :P
+	debug = false;
+
+	$records;
+
+	/**
+	 * Creates a new instance of the NDEDatabase class.
+	 *
+	 * When an instance of this class is created, we need to:
+	 * 1. Load the index file (basefile + '.idx').
+	 * 2. Load the data file (basefile + '.dat').
+	 * 3. Loop through all the indices, and load the corresponding data.
+	 */
+	constructor ($basefile) {
+	this.index = new NDEIndex($basefile + '.idx');
+	this.data = new NDEData($basefile + '.dat');
+
+	// Need to read in all the records
+	for ($i = 0; $i < this.index.record_count(); $i++) {
+		// Get the next index from the index file
+		$index_data = this.index.get();
+		// NDEDatabase::debug_msg('<strong>Read ' . $i . ':</strong> offset = ' . $index_data['offset'] . ', index = ' . $index_data['index'] . '... <br />');
+		// Now, get the data associated with this index.
+		$data = this.data.get_record($index_data['offset'], $index_data['index']);
+		// Was it a record? If so, process it.
+		if ($data instanceof NDERecord) {
+			this._process_record($data);
+		}
+	}
+}
+
+	/**
+	 * Add a record to our array. Defined as a function so that other classes
+	 * can extend it.
+	 */
+	_process_record($record) {
+	this.records[] = $record;
+	// Return the index of the new record.
+	return count(this.records) - 1;
+}
+
+	/**
+	 * Getter method for $songs. Returns the songs in the database.
+	 */
+	records() {
+	return this.records;
+}
+
+	/**
+	 * Debugging function, used to show debug messages only if we're in debug
+	 * mode.
+	 */
+	debug_msg($string) {
+		if (this.debug) {
+			console.log($string);
+		}
+	}
+};
+
+exports = {
+	NDEIndex,
+	NDEDatabase,
+	NDEData
+};
